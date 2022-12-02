a3bd36a1b0eafdc52ea126ec244f7fd032bd710a
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-02 23:17:22 +0100

    coordenadas de los muros

--- lab.py
+++ lab.py
@@ -15,5 +15,6 @@ def laberinto(dimension, muros):
                 fila.append(' ')
         laberinto.append(fila)
     return laberinto
+muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3)) 
 
 
