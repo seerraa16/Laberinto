d416dd5e011ce4f7520e24dfe713b8815454b8d6
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2022-12-02 23:16:35 +0100

    establecemos dimensiones

--- lab.py
+++ lab.py
@@ -6,5 +6,14 @@ def laberinto(dimension, muros):
     [' ', ' ', ' ', 'X', ' '], 
     ['X', 'X', 'X', 'X', 'S']
     ]
+    for i in range(dimension):
+        fila = []
+        for j in range(dimension):
+            if tuple([i, j]) in muro:
+                fila.append('X')
+            else:
+                fila.append(' ')
+        laberinto.append(fila)
+    return laberinto
 
 
